b692973f614c750943455306f62a09e6aef2f5f5
xitya <1556327376@qq.com>
2022-03-30 16:04:45 +0800

    其他入库历史

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,7 @@ const routes = [
     {path:"/stock",component:()=>import('../components/navigationbar.vue'),
         children:[
             {path:"/otherOutStock",component:()=>import('../components/stock/OtherOutStock.vue')},
+            {path:"/Warehousing",component:()=>import('../components/stock/Warehousing.vue')},
             // {path:"/user",component:()=>import('../components/sys/user.vue')},
             // {path:"/role",component:()=>import('../components/sys/role.vue')},
             // {path:"/financing/collection",component:()=>import('../components/financing/collection.vue')},
