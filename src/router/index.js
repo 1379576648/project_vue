61b10b9feeaaf2598ff67dc2ec2f43f3177575ee
彭紫炎 <1269353044@qq.com>
2022-03-30 19:38:09 +0800

    彭紫炎

--- src/router/index.js
+++ src/router/index.js
@@ -37,8 +37,11 @@ const routes = [
             {path:"/otherOutStock",component:()=>import('../components/stock/OtherOutStock.vue')},
             {path:"/otherOutStockAll",component:()=>import('../components/stock/OtherOutStockAll.vue')},
             {path:"/otherOutStockDetails",component:()=>import('../components/stock/OtherOutStockDetails.vue')},
-            {path:"/Warehousing",component:()=>import('../components/stock/Warehousing.vue')}
-
+            {path:"/Warehousing",component:()=>import('../components/stock/Warehousing.vue')},
+            //库存查询
+            {path:"/Management",component:()=>import('../components/stock/Management.vue')},
+            //仓库管理
+            {path: "/Warehouse",component:()=>import('../components/stock/Warehouse.vue')}
             // {path:"/user",component:()=>import('../components/sys/user.vue')},
             // {path:"/role",component:()=>import('../components/sys/role.vue')},
             // {path:"/financing/collection",component:()=>import('../components/financing/collection.vue')},
