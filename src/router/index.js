61957d197a070f999a1ad2a7cc83d6f77fb91acd
袁可颖 <3043949730@qq.com>
2022-03-30 15:40:51 +0800

    完善

--- src/router/index.js
+++ src/router/index.js
@@ -15,9 +15,13 @@ const routes = [
             {path:"/workbench",component:()=>import('../components/workbench_management/workbench_main.vue')},
             {path:"/user",component:()=>import('../components/sys/user.vue')},
             {path:"/role",component:()=>import('../components/sys/role.vue')},
+            //收款历史
             {path:"/financing/collection",component:()=>import('../components/financing/collection.vue')},
+            //付款单
             {path:"/financing/payment",component:()=>import('../components/financing/payment.vue')},
+            //付款历史
             {path:"/financing/payment_history",component:()=>import('../components/financing/payment_history.vue')},
+            //付款详情
             {path:"/financing/payment_details",component:()=>import('../components/financing/payment_details.vue')},
         ]
     },
