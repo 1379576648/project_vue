165b1d0a255a1db7936c426ffc6ae37469c7271d
袁可颖 <3043949730@qq.com>
2022-03-30 09:03:30 +0800

    完善

--- src/store/index.js
+++ src/store/index.js
@@ -167,8 +167,8 @@ const store = createStore({
                         MENU_ID: 6.5,//菜单编号
                         MENU_NAME: '付款单',//菜单名称
                         PICTURE_ADDRESS: '&#xe64c;',//图片地址
-                        MENU_ROUTE: 'financing/message/receiving',//路由地址
-                        MENU_MODULE: '/components/financing/',//组件地址
+                        MENU_ROUTE: '/financing/payment',//路由地址
+                        MENU_MODULE: '/components/financing/payment',//组件地址
                         MENU_STATE: 0,//是否启用 0启用 1禁用
                         MENU_TYPE: 0,//菜单类型 0:菜单 1:按钮
                         MENU_LEAF: 1,//是否有叶子 0有 1没有
