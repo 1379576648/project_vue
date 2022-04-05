f8b0be5ff5463971ffa24f2871ce4a3000d46b85
tanwei <1713984131@qq.com>
2022-04-05 15:00:43 +0800

    变更

--- src/components/commodity/details.vue
+++ src/components/commodity/details.vue
@@ -1,81 +1,134 @@
 <template>
-  <div class="det_body_1">
-    <div class="btn_sp">
-      <el-button><router-link to="goin">商品列表</router-link></el-button>
-    </div>
-    <div class="det_body_2" style="margin:0px;">
+  <div style="width: 100%;">
+    <div id="scroll" style="max-height:calc(100vh - 100px) ">
+      <div class="ant-card">
+        <div class="ant-card-head" style="border-bottom: 1px dashed rgb(217, 217, 217); padding: 24px 12px 0px;">
+          <div class="ant-card-head-wrapper">
+            <div class="ant-card-head-title">
+              <div class="line-height-36 ant-row">
 
-      <div class="det_body_3">
+                <div class="ant-col ant-col-8">
+                  <span class="font-size-24 font-bold-700"  align="center">销售详情</span>
+                </div>
 
-        <div class="det_cl">
-          <div style="float:left;margin-left: 100px;margin-top: 10px" class="edt_font">{{this.tatadata.commodityName}}</div>
-        </div>
 
-          <span class="det_zs"  style="float:left;margin-left: 100px">在售</span>
-          <br>
-          <div class="det_cl" >
-            <br>
 
-          <div class="det_bh"  style="float:left;margin-left: 100px;width: 15%;text-align: left">
-           <span>编号&nbsp;:&nbsp;&nbsp;</span>
-           <span>{{this.tatadata.commodityId}}</span>
+                <div class="ant-col ant-col-8" align="right">
+                  <span style="color: #a5a5a5;">单据编号：</span>
+                  <span>{{Fromlist.bills}}</span>
+                </div>
+              </div>
+            </div>
           </div>
-
-            <div class="det_bh"  style="float:left;margin-left: 100px;width: 15%;text-align: left">
-              <span>分类&nbsp;:&nbsp;&nbsp;</span>
-              <span>{{this.tatadata.categoryId}}</span>
+        </div>
+        <div class="ant-card-body">
+          <div class="ant-row_1">
+            <div class="ant-row_2">
+              <span style="font-size: 16px">仓库：</span>
+              &nbsp;&nbsp;&nbsp;
             </div>
-
-<!--            <div class="det_bh"  style="float:left;margin-left: 100px;width: 15%;text-align: left">-->
-<!--              <span>条形码&nbsp;:&nbsp;&nbsp;</span>-->
-<!--              <span>68032608506</span>-->
-<!--            </div>-->
-
-            <div class="det_bh"  style="float:left;margin-left: 100px;width: 25%;text-align: left">
-              <span>单位&nbsp;:&nbsp;&nbsp;</span>
-              <span>{{this.tatadata.commoditySpecifications}}</span>
-            </div>
-
-            <div class="det_bh"  style="float:left;margin-left: 100px;width: 100%;text-align: left" >
-              <span>规格&nbsp;:&nbsp;&nbsp;</span>
-              <span>{{this.tatadata.commodityCompany}}</span>
-            </div>
-            <div class="det_bh"  style="float:left;margin-left: 100px;width: 100%;text-align: left" >
-              <span>备注&nbsp;:&nbsp;&nbsp;</span>
-              <span>无</span>
-              <div class="productPrice">
-                <span>零售价&nbsp;:&nbsp;&nbsp;</span>
-                <span style="color: #1dcf6a;">￥{{this.tatadata.retailPrice}}</span>
-                <span>/{{this.tatadata.commoditySpecifications}}</span>
-                <br>
-                <span>进货价&nbsp;:&nbsp;&nbsp;</span>
-                <span style="color: #1dcf6a;">￥{{this.tatadata.tradePrice}}</span>
-                <span>/{{this.tatadata.commoditySpecifications}}</span>
-                <br>
-                <span>最低售价&nbsp;:&nbsp;&nbsp;</span>
-                <span style="color: #1dcf6a;">￥{{this.tatadata.retailPrice}}</span>
-                <span>/{{this.tatadata.commoditySpecifications}}</span>
-                <br>
-                <span>批发价&nbsp;:&nbsp;&nbsp;</span>
-                <span style="color: #1dcf6a;">￥{{this.tatadata.tradePrice}}</span>
-                <span>/{{this.tatadata.commoditySpecifications}}</span>
-                <br>
-
+            <div class="ant-row_2" style="display: flex">
+              <div>
+                <span style="font-size: 16px">业务日期：</span>
               </div>
 
             </div>
-
-            <br>
-
-
-
-
           </div>
+          <div>
+            <div class="mytable-style vxe-cell-hide vxe-table tid_1 size--mini border--full vxe-editable show--head show--foot has--height fixed--right t--animat row--highlight">
+
+              <el-table
+                  :data="tableData"
+                  border
+                  style="width: 100%; font-size: 12px"
+                  :header-cell-style="{background:'#F8F8F9',color:'#606266'}"
+                  :default-sort="{ prop: 'name', order: 'descending' }"
+              >
+                <el-table-column label="序号" type="index" width="50" />
+                <el-table-column  label="商品名称" align="center" width="320px">
+                </el-table-column>
+                <el-table-column prop="name" label="商品编号" />
+                <el-table-column prop="name" label="规格/属性" />
+                <el-table-column prop="name" label="单位" />
+                <el-table-column prop="name" label="商品编号" />
+                <el-table-column prop="name" label="数量" />
+                <el-table-column prop="name" label="商品金额(元)" />
+                <el-table-column prop="name" label="备注" />
+                <el-table-column label="操作" width="90" align="center">
+                  <template #default="scope">
+                    <el-button type="text" size="small" @click="delData(scope.$index, scope.row)">删除</el-button>
+                    <el-button type="text" size="small" @click="addhang()">添加</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+
+            </div>
+            <div style="height: 40px;width: 100%;margin-top: 12px;display: flex;align-items: center;font-weight: bold">
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">销售总额：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">整单折扣：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">整单优惠：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">其他费用：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+            </div>
+            <div style="height: 40px;width: 100%;display: flex;align-items: center;font-weight: bold">
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">应收金额：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">应收金额：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">实收金额：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+              <div class="ant_from">
+                <div>
+                  <label title="销售总额">累加欠款：</label>
+                  <span class="pl12">0.00元</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
 
       </div>
     </div>
+    <div style="height: 230px;width: 100%;background-color: white">
+      <div style="padding: 10px;width: 100%;height: 60px;">
+        <el-button style="float: right">返回</el-button>
 
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -83,101 +136,198 @@ export default {
   name: "details",
   data(){
     return{
-      tatadata:{},
+      selectGoodsDialog:false,
+      input:'',
+      tableData: [
+        {name:''},
+        {name:''},
+        {name:''}
+      ],
+      tableData1:[],
+      Fromlist:{
+        businessdate:'', //业务时间
+        bills:'', //单据编号
+      },
     }
-  },methods:{
-    /*
-   * 根据id查询 商品表
-   * */
-    selectById(){
-      this.axios
-          .get("http://localhost:9090/commodity/selectById/" + this.$route.query.id)
-          .then((response => {
-            console.log(response);
-            this.tatadata = response.data;
-            console.log(this.tatadata)
-          })).catch(function (error) {
-        console.log(error);
-      })
-    }
-  },created() {
-    this.selectById()
   }
 }
 </script>
 
 <style scoped>
-.edt_font{
-  margin-bottom: 10px;
-  font-weight: 700;
-  font-size: 18px;
-  box-sizing: border-box;
-  color: rgba(0,0,0,.65);
-  font-variant: tabular-nums;
-  line-height: 1.5;
-  list-style: none;
-  font-feature-settings: "tnum";
+.pl12{
+  padding-left: 12px;
 }
-.det_body_1{
-  color: rgba(0,0,0,.65);
-  font-size: 14px;
-  font-variant: tabular-nums;
-  line-height: 1.5;
-  list-style: none;
-  font-feature-settings: "tnum";
+.ant_from{
+  width: 270px;
 }
-a{
-  text-decoration:none ;
-  color: #409eff;
+.ant_sp{
+  display: none;
 }
-.det_zs{
-  color: #52c41a;
-  background: #f6ffed;
-  border-color: #b7eb8f;
-  border: 1px solid #d9d9d9;
+.el-table--enable-row-hover .el-table__body tr:hover .ant_sp{
+  display: block;
+}
+
+.vxe-table.size--mini {
   font-size: 12px;
-  line-height: 20px;
-  white-space: nowrap;
-  background: #fafafa;
-  border: 1px solid #d9d9d9;
-  border-radius: 0;
-  cursor: default;
-  opacity: 1;
-  height: auto;
-  margin-right: 8px;
-  padding: 0 7px;
-  float: left;
 }
-.det_bh{
-  color: rgba(0,0,0,.65);
-  font-size: 14px;
-  font-variant: tabular-nums;
-  line-height: 1.5;
-  list-style: none;
-  font-feature-settings: "tnum";
+.ant-card-body{
+  min-height: 350px;
+  padding: 12px;
 }
-.det_cl{
-  float: left;
+.ant-row_1{
+  display: flex;
+  height: 48px;
   width: 100%;
 }
-.det_body_2{
-  width: 93%;
-  height: 340px;
-  border: 1px solid #ddd;
-  background: white;
+.ant-row_2{
+  width: 50%;
 }
-.btn_sp{
-  position: absolute;
-  right: 145px;
-  top:90px;
+.iconfont{
+  font-family:"iconfont" !important;
+  font-size:19px;font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;}
 
+.ant-card-head {
+  min-height: 48px;
+  margin-bottom: -1px;
+  color: rgba(0,0,0,.85);
+  font-weight: 500;
+  font-size: 16px;
+  background: transparent;
+  border-bottom: 1px solid #e8e8e8;
+  border-radius: 2px 2px 0 0;
+  zoom: 1;
+  border-bottom: 1px dashed rgb(217, 217, 217);
+  padding: 24px 12px 0px;
 }
-.productPrice{
-  background: #e5e5e5;
-  padding: 12px;
-  width: 85%;
-/*  height: 20px;*/
-  margin-top: 10px;
+.ant-card {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  color: rgba(0,0,0,.65);
+  font-size: 14px;
+  font-variant: tabular-nums;
+  line-height: 1.5;
+  list-style: none;
+  position: relative;
+  background: #fff;
+  border-radius: 2px;
+}
+.ant-card-head:after, .ant-card-head:before {
+  display: table;
+  content: "";
+}
 
+*, :after, :before {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
+.ant-card-head:after {
+  clear: both;
+}
+.ant-card-head:after, .ant-card-head:before {
+  display: table;
+  content: "";
+}
+*, :after, :before {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.ant-card-head-wrapper {
+  display: flex;
+  align-items: center;
+}
+*, :after, :before {
+  box-sizing: border-box;
+}
+.ant-card-head-title {
+  display: inline-block;
+  -webkit-box-flex: 1;
+  flex: 1;
+  padding: 16px 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.ant-row {
+  position: relative;
+  height: auto;
+  margin-right: 0;
+  margin-left: 0;
+  zoom: 1;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.line-height-36 {
+  line-height: 36px;
+}
+.ant-col-8 {
+  display: block;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  width: 33.33333333%;
+}
+.ant-col-8{
+  -webkit-box-flex: 0;
+  flex: 0 0 auto;
+  float: left;
+}
+.ant-col-8{
+  position: relative;
+  padding-right: 0;
+  padding-left: 0;
+}
+.font-bold-700 {
+  font-weight: 700;
+}
+.font-size-24 {
+  font-size: 24px;
+}
+/deep/.el-input-group__append, .el-input-group__prepend {
+  background-color: var(--el-fill-color-light);
+  color: var(--el-color-info);
+  vertical-align: middle;
+  display: table-cell;
+  position: relative;
+  border-radius: var(--el-input-border-radius);
+  padding: 0 10px;
+  width: 1px;
+  white-space: nowrap;
+}
+.input-with-select span{
+  cursor: pointer;
+}
+
+
+
+
+
+
+
+
+
+
+.el-pagination .el-select .el-input {
+  width: 90px;
+}
+#scroll{
+  overflow:scroll;
+  overflow-x: hidden;
+}
+#scroll::-webkit-scrollbar{
+  width:5px;
+}
+#scroll::-webkit-scrollbar-thumb{
+  background:linear-gradient(rgb(17,157,212),rgb(17,212,105));
+  border-radius:5px;
+  transition:1s;
+}
+#scroll::-webkit-scrollbar-thumb:hover{
+  background:linear-gradient(rgb(17,212,105),rgb(17,157,212));
+}
+
+
 </style>
